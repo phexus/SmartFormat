f1c84cdef45c33b03e8382020de85a702a65b283
phexus <phexus@163.com>
2024-07-07 01:25:31 +0800

    Add EvaluateSingleSelector

---
+++ src/SmartFormat/Evaluator.Extension.cs
@@ -0,0 +1,33 @@
+﻿//
+// Copyright SmartFormat Project maintainers and contributors.
+// Licensed under the MIT license.
+//
+
+using System.Linq;
+using SmartFormat.Core.Formatting;
+using SmartFormat.Core.Parsing;
+
+namespace SmartFormat;
+internal partial class Evaluator
+{
+    public object? EvaluateSingleSelector(FormattingInfo formattingInfo)
+    {
+        var format = formattingInfo.Format!;
+
+        OnFormat?.Invoke(this, new FormatEventArgs(format));
+
+        var item = format.Items.Single();
+        if (item is LiteralText literalItem)
+        {
+            return literalItem.BaseString;
+        }
+
+        // Otherwise, the item must be a placeholder.
+        var placeholder = (Placeholder) item;
+        var childFormattingInfo = formattingInfo.CreateChild(placeholder);
+        OnPlaceholder?.Invoke(this, new PlaceholderEventArgs(placeholder));
+
+        // Try to get a value for the placeholder...
+        return EvaluatePlaceholder(childFormattingInfo) ? childFormattingInfo.CurrentValue : null;
+    }
+}
---
+++ src/SmartFormat/SmartFormatter.Extension.cs
@@ -0,0 +1,49 @@
+﻿//
+// Copyright SmartFormat Project maintainers and contributors.
+// Licensed under the MIT license.
+//
+
+using System;
+using System.Collections.Generic;
+using SmartFormat.Core.Output;
+using SmartFormat.Core.Parsing;
+using SmartFormat.Pooling.SmartPools;
+using SmartFormat.ZString;
+
+namespace SmartFormat;
+
+/// <summary>
+/// Smart formatter extension
+/// </summary>
+public static class SmartFormatterExtension
+{
+    /// <summary>
+    /// Evaluate single selector.
+    /// </summary>
+    /// <param name="smartFormatter"></param>
+    /// <param name="formatParsed"></param>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static object? EvaluateSingleSelector(this SmartFormatter smartFormatter, Format formatParsed, params object?[] args)
+    {
+        // The first item is the default and will be used for the action,
+        // but all args go to FormatDetails.OriginalArgs
+        var current = ((IList<object?>) args).Count > 0 ? args[0] : args; // The first item is the default.
+
+        using var zsOutput = new ZStringOutput(ZStringBuilderUtilities.CalcCapacity(formatParsed));
+        using var fdo = FormatDetailsPool.Instance.Pool.Get(out var formatDetails);
+        formatDetails.Initialize(smartFormatter, formatParsed, args, null, zsOutput);
+
+        using var fio = FormattingInfoPool.Instance.Pool.Get(out var formattingInfo);
+        formattingInfo.Initialize(formatDetails, formatParsed, current);
+
+        smartFormatter.Registry.ThrowIfNoExtensions();
+        if (formattingInfo.Format is null) return null;
+
+        if (formattingInfo.Format.Items.Count != 1) throw new InvalidOperationException(
+            "There are more than one selector!");
+
+        return smartFormatter.Evaluator.EvaluateSingleSelector(formattingInfo);
+    }
+}
